943f101feeca72d3df1a7a3ed3acfae057f43718
Andrew Navratil <drew.navratil@gmail.com>
2026-07-22 06:17:59 -0400

    add a controller

---
+++ mslearn/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class  HelloWorldController : Controller
+{
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "default action";
+    }
+
+    // GET: /HelloWorld/Welcome
+    public string Welcome(string name, int ID = 1)
+    {
+        // return "welcome method";
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+    }
+    // public string Welcome(string name, int numTimes = 1)
+    // {
+    //     // return "welcome method";
+    //     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    // }
+}
