bfbaecca5448ff6bb726986be1324c861f57ef7e
Andrew Navratil <drew.navratil@gmail.com>
2026-07-23 05:06:54 -0400

    add a search

--- mslearn/MvcMovie/Controllers/MoviesController.cs
+++ mslearn/MvcMovie/Controllers/MoviesController.cs
@@ -20,9 +20,37 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            // return View(await _context.Movie.ToListAsync());
+
+            if (_context.Movie == null)
+            {
+                return Problem("entity set 'MvcMovieContext.Movie' is null");
+            }
+
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
+            var movies = from m in _context.Movie select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+            // return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
