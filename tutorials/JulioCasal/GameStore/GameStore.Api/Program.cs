3d8299d9800c14c188b8368658dc7a0edea5158b
Andrew Navratil <drew.navratil@gmail.com>
2026-07-27 05:55:05 -0400

    read/get implementations inline

--- tutorials/JulioCasal/GameStore/GameStore.Api/Program.cs
+++ tutorials/JulioCasal/GameStore/GameStore.Api/Program.cs
@@ -1,6 +1,18 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992,7,15)),
+    new (2, "Final Fantasy VII Rebirth", "RPG", 69.99M, new DateOnly(2024,2,29)),
+    new (3, "Astro Bot", "Platformer", 59.99M, new DateOnly(2024,9,6))
+];
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/{id}
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
