3f586d1d463108a1c74e9ca8290023e3aabeae0b
Andrew Navratil <drew.navratil@gmail.com>
2026-07-28 01:58:25 -0400

    footballleage (ef core): ef migrations initial create

--- tutorials/TrevoirWilliams/Data/FootballLeagueDbContext.cs
+++ tutorials/TrevoirWilliams/Data/FootballLeagueDbContext.cs
@@ -1,12 +1,23 @@
 using EFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EFCore.Data;
 
 public class FootballLeagueDbContext : DbContext
 {
-    public FootballLeagueDbContext(DbContextOptions<FootballLeagueDbContext> options) : base(options)
-    {}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+            // .SetBasePath(Directory.GetCurrentDirectory())
+            // .AddJsonFile("appsettings.json")
+            .AddUserSecrets<FootballLeagueDbContext>()
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("FootballLeague");
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 
     public DbSet<Team> Teams { get; set; }
     public DbSet<League> Leagues { get; set; }
